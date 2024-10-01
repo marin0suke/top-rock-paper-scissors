3546eafa24d954c2c8e6bdf43c48a029985e5b28
marin0suke <110573250+marin0suke@users.noreply.github.com>
2024-10-01 10:41:01 +1000

    Add playRound function and return winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,21 +2,45 @@
 
 let getComputerChoice = () => {
     let choice = Math.floor((Math.random() * 3) + 1); // Math floor makes every mathrandom 0. * 3 to get floors 0, 1 or 2. for readability, can add + 1 so we get 1, 2, 3. 
-    return (choice == 1) ? "rock" : (choice == 2) ? "paper" : "scissors"; //
+    console.log(choice);
+    return (choice == 1) ? "rock" : (choice == 2) ? "paper" : "scissors";
 };
 
-console.log(getComputerChoice());
-
 let getHumanChoice = () => {
-    let choice; // declares var and sets it to true.
-    while (true) {
-        choice = prompt("rock, paper or scissors?").toLowerCase();
-        if (choice === "rock" || choice === "paper" || choice === "scissors") { // not sure if need strict here..
+        let choice = prompt("rock, paper or scissors?").toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") { 
+            console.log(choice);
             return choice;
         } else {
-            alert("please type rock, paper, or scissors."); // how to loop? 
+            alert("please type rock, paper, or scissors.");
         };
-    }
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) { // edge cases.
+        alert("It's a draw!");
+        return "draw";
+    }
+
+    // initialise var winner to return from this function so we can console.log the winner.
+    let winner = // if we need the outcome in a package to display or input into another func - init variable.
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper") 
+        ? "human"
+        : "computer";
+
+        //display
+    alert(`${winner === "human" ? "You" : "Computer"} won this round!`);
+
+    return winner;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
